e15bc8c56ff28bfc5beaac5d05e6375f670468ef
elise <elise62110@gmail.com>
2026-01-09 17:09:17 +0100

    commit

---
+++ p00/ex5/ft_count_harvest_recursive.py
@@ -0,0 +1,13 @@
+def ft_count_harvest_recursive():
+	try:
+		total_days = int(input("Days until harvest: "))
+	except EOFError:
+		return
+	def count(current):
+		if current > total_days:
+			print("Harvest time!")
+			return
+		print(f"Day {current}")
+		count(current + 1)
+	count(1)
+ft_count_harvest_recursive()
--- p00/ex5/ft_count_hervest_iterative.py
+++
@@ -1,3 +0,0 @@
-def ft_count_harvest_iterative():
-    days = int(input("Days until harvest: "))
-    
