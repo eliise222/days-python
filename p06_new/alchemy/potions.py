b7edba6b6f4aba1b4f1f448c1b90aa67ee8f44a6
Elise Locufier <elocufie@c3r1p1.42nice.fr>
2026-04-24 13:48:13 +0200

    commit

--- p06_new/alchemy/potions.py
+++ p06_new/alchemy/potions.py
@@ -3,10 +3,10 @@ import elements
 
 
 def healing_potion():
-    return f"Healing potion brewerd with '{el.create_earth()}' \
+    return f"Healing potion brewed with '{el.create_earth()}' \
 and '{el.create_air()}'"
 
 
 def strength_potion():
-    return f"Strength potion brewerd with '{elements.create_fire()}' \
+    return f"Strength potion brewed with '{elements.create_fire()}' \
 and '{elements.create_water()}'"
